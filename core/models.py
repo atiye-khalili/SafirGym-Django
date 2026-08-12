951fb549c03848afd753e333ec2c81e7e0680886
ABAN <aban@D.P.D>
2026-08-12 11:54:54 +0330

    add url

--- core/models.py
+++ core/models.py
@@ -13,23 +13,12 @@ class Register(models.Model):       #1
     has_medical_condition = models.BooleanField(verbose_name='دارای شرایط پزشکی')
     describe_condition= models.TextField(verbose_name='شرح شرایط پزشکی')
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
     class Meta:
         verbose_name = 'ثبت نام'
         verbose_name_plural = 'ثبت نام'
-
-
-class Trainer(models.Model):        #2
-    first_name = models.CharField(max_length=30, verbose_name='نام')
-    last_name = models.CharField(max_length=30, verbose_name='نام خانوادگی')
-    birthdate = models.DateTimeField(verbose_name='تاریخ تولد')
-    phone_number = models.CharField(max_length=11, verbose_name='شماره تلفن')
-    specialization = models.CharField(max_length=50, verbose_name='تخصص')
-    experience = models.CharField(max_length=50, verbose_name= 'سابقه کاری(سال)')
-    biography = models.TextField(verbose_name='بیوگرافی')
-
-    class Meta:
-        verbose_name = 'مربی'
-        verbose_name_plural = 'مربیان'
 
 
 class ClassNameChoices(models.TextChoices):
@@ -39,13 +28,31 @@ class ClassNameChoices(models.TextChoices):
     FITNESS = ('fitness', 'فیتنس')
     TRX = ('trx', 'تی آر ایکس')
     CROSSFIT = ('crossfit', 'کراسفیت')
-    
+
+class Trainer(models.Model):        #2
+    first_name = models.CharField(max_length=30, verbose_name='نام')
+    last_name = models.CharField(max_length=30, verbose_name='نام خانوادگی')
+    birthdate = models.DateField(verbose_name='تاریخ تولد')
+    phone_number = models.CharField(max_length=11, verbose_name='شماره تلفن')
+    specialization = models.CharField(max_length=50,choices=ClassNameChoices.choices ,verbose_name='تخصص')
+    experience = models.CharField(max_length=50, verbose_name= 'سابقه کاری(سال)')
+    biography = models.TextField(verbose_name='بیوگرافی', null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} : {self.specialization}"
+    class Meta:
+        verbose_name = 'مربی'
+        verbose_name_plural = 'مربیان'
+
+
 class GymClass(models.Model):       #3
     class_name = models.CharField(max_length=50, choices=ClassNameChoices.choices)
     trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
     capacity = models.PositiveSmallIntegerField(verbose_name='ظرفیت کلاس')
     schedule = models.CharField(max_length=100, verbose_name='برنامه زمانی کلاس')
 
+    def __str__(self):
+        return f"{self.class_name} : {self.trainer.first_name} {self.trainer.last_name}"
     class Meta:
         verbose_name = 'کلاس'
         verbose_name_plural = 'کلاس‌ها'
@@ -56,6 +63,8 @@ class News(models.Model):
     content = models.TextField(verbose_name='محتوا')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='ایجاد شده در تاریخ')
 
+    def __str__(self):
+        return self.title
     class Meta:
         verbose_name = 'خبر'
         verbose_name_plural = 'اخبار'
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from core.models import *
 
-# Create your views here.
+# def gym(request):
+#     return render(request, 'core/index.html')
+
+def home(request):
+    name = Register.objects.first()
+    context={
+        'firstname' : name
+    }
+    return render(request, 'core/index.html', context=context)
