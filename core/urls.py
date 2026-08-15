870a033078b680c1b0576782477005acd2eac8e0
ABAN <aban@D.P.D>
2026-08-15 16:44:07 +0330

    add bootstrap

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from  core.views import *
 urlpatterns = [
     path('home/' , home, name='home'),
     path('news/' , news, name='news'),
-    path('gymclass/' , gymclass, name='gymclass')
+    path('gymclass/' , gymclass, name='gymclass'),
+    path('trainer/' , trainer, name='trainer'),
+    path('trainer/detail/<int:trainer_id>/', trainer_detail, name='trainer_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from core.models import *
 
 
 def home(request):
     name = Register.objects.first()
-    context={
+    return render(request, 'core/home.html', context={
         'firstname' : name.first_name
-    }
-    return render(request, 'core/home.html', context=context)
+    })
 
 def news(request):
     news = News.objects.last()
-    context= {
+    # context= {
+    #     'news_title': news.title
+    # }
+    return render(request, 'core/news.html', context={
         'news_title': news.title
-    }
-    return render(request, 'core/news.html', context=context)
+    })
 
 def gymclass(request):
     classes = GymClass.objects.all()
-    context={
-        'classes' : classes.classes.class_name
-    }
-    return render(request, 'core/gymclass.html', context=context)
+    return render(request, 'core/gymclass.html', context={
+        'classes' : classes
+    })
+
+def trainer(request):
+    trainer = Trainer.objects.all()
+    return render(request, 'core/trainer.html', context={
+        'trainer' : trainer
+    })
+
+def trainer_detail(request, trainer_id):
+    detail= Trainer.objects.get(id=trainer_id)
+    return render(request, 'core/trainer_detail.html', context={'detail': detail})
